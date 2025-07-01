b3c0be3d0f585244df75f8a73f61c73c2493b148
Dean Mark <deansy0718@gmail.com>
2025-07-01 19:44:49 +0800

    learned that functions can store in a variable

---
+++ Lessons/12-advance-functions.js
@@ -0,0 +1,10 @@
+function greet() {
+    console.log('hi!')
+}
+greet();
+//FUNCTION CAN STORE IN A VARIABLE
+const func = function greetings() { 
+    console.log('hello!');
+};
+//You can also call it directly using ()
+func()
