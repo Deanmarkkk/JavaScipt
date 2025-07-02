a2fe92f22a365a9e085e704c8dfbac431c728ee2
Dean Mark <deansy0718@gmail.com>
2025-07-02 13:31:14 +0800

    learned advanced function basics

--- Lessons/12-advance-functions.js
+++ Lessons/12-advance-functions.js
@@ -1,10 +1,30 @@
-function greet() {
-    console.log('hi!')
-}
-greet();
-//FUNCTION CAN STORE IN A VARIABLE
-const func = function greetings() { 
-    console.log('hello!');
+//FUNCTION CAN STORE IN A VARIABLE, CALLED "ANONYMOUS FUNCTION"
+const funcGreet = function () {
+    console.log('Hi there!');
 };
 //You can also call it directly using ()
-func()
+funcGreet();
+
+const obj1 = {name: 'dean',
+     todo: function () { //called method, function inside an object
+    console.log('wash dishes')
+}};
+console.log(obj1.name)
+obj1.todo()
+//PASSING A VALUE INTO A FUNCTION
+function display(param) {
+    console.log(param);
+}
+display(5);
+//PASSING FUNCTION INTO ANOTHER FUNCTION
+function practice1(param) {
+    param() //because param contains function, we can use ()
+};
+practice1(function run() {console.log('hello there!')})
+//setTimeout() FUNCTION
+setTimeout(function greet(){console.log('hi there!')}, 2000);//1st parameter, how long to display in millisecond
+
+//setInterval() FUNCTION, It keeps running every second
+setInterval(function greet1(){console.log('morning')}, 3000); //same as setTimeout() function
+//ASYNCHRONOUS CODE
+console.log('time out!') //the setTimeout() function will display next to this line
