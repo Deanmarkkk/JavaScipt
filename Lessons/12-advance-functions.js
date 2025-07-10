3006d139677f9e95cc26c3c04472b5cc9d95e879
Dean Mark <deansy0718@gmail.com>
2025-07-10 19:40:30 +0800

    Arrow Functions - unfinished

--- Lessons/12-advance-functions.js
+++ Lessons/12-advance-functions.js
@@ -32,9 +32,34 @@ console.log('time out!') //the setTimeout() function will display next to this l
 const arrayForeach = [
     'dean', 'mikhail', 'john', 'migs'
 ];
-arrayForeach.forEach(function (value, i) { //save the 1-last value to the value, the index number
+arrayForeach.forEach(function (value) { //save the 1-last value to the value
     if (value === 'dean') {
         return value;
     }
     console.log(value)
 });
+
+//-------------------------------------------------------------------------------
+//ADVANCED FUNCTIONS PART 2
+
+//ARROW FUNCTIONS
+const arrowFunc = (param, param1) => {
+
+    if (param === param1) {
+        console.log('same input!')
+    } else {
+        console.log('Not same input!')
+    }
+};
+arrowFunc('dean', 'dean');
+//YOU CAN ALSO REMOVE THE BRACKETS IN PARAMETER, ITS WORKS THE SAME
+const oneParameter = one => { 
+    console.log(one + 5);
+};
+oneParameter(5);
+//ONE LINE ARROW FUNCTION - SHORTCUT
+//can also remove curly brackets and the return command
+const oneLine = () => 3 + 5;
+setTimeout(function () {
+    return console.log(`One line function: ${oneLine()}`)
+}, 1000);
